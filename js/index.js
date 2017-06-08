5f4e46cbcbd4443b9649e8163b064ba7336db530
monkey <595632060@qq.com>
2017-06-08 21:46:56 +0800

    js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 'use strict';
 angular.module('app',['ui.router','validation','ngCookies']);
 angular.module('app').value('dict',{}).run(['$http','dict',function($http,dict){
-  $http.get('/data/city.json').then(function(resp){
+  $http.get('data/city.json').then(function(resp){
     dict.city=resp.data;
   });
-  $http.get('/data/salary.json').then(function(resp){
+  $http.get('data/salary.json').then(function(resp){
     dict.salary=resp.data;
   });
-  $http.get('/data/scale.json').then(function(resp){
+  $http.get('data/scale.json').then(function(resp){
     dict.scale=resp.data;
   });
 }])
@@ -98,6 +98,201 @@ required:function(value){
     }
   };
   $validationProvider.setExpression(expression).setDefaultMsg(defaultMsg);
+}])
+angular.module('app')
+.controller('companyCtrl',['$http','$state','$scope',function($http,$state,$scope){
+  $http.get('data/company.json?id='+$state.params.id).then(function(resp){
+    $scope.company=resp.data;
+  })
+}])
+angular.module('app').controller('favoriteCtrl',['$http','$scope',function($http,$scope){
+  $http.get('data/myFavorite.json').then(function(resp){
+    var res=resp.data;
+    $scope.list=res;
+  })
+}])
+angular.module('app').controller('loginCtrl',['$http','$scope','$state','cache',function($http,$scope,$state,cache){
+  $scope.submit=function(){
+    $http.post('data/login.json',$scope.user).then(function(resp){
+      var res=resp.data;
+      cache.put('id',res.id);
+      cache.put('name',res.name);
+      cache.put('image',res.image);
+      $state.go('main');
+    })
+  }
+}])
+angular.module('app')
+.controller('mainCtrl',['$scope','$http',function($scope,$http){
+  $http.get('data/positionList.json').then(function(resp){
+    console.log(resp);
+    $scope.list=resp.data;
+  })
+  // $scope.list=[{
+  //   name:'销售',
+  //   companyName:'千度',
+  //   imgSrc:'image/company-3.png',
+  //   city:'上海',
+  //   industry:'互联网',
+  //   time:'2017-4-10 11:05',
+  //   id:'1'
+  // },{
+  //   id:'2',
+  //  name:'WEB前端',
+  //   companyName:'慕课网',
+  //   imgSrc:'image/company-1.png',
+  //   industry:'互联网',
+  //   city:'北京',
+  //   time:'2017-4-5 01:10'
+  // }];
+}])
+angular.module('app').controller('meCtrl',['$state','cache','$http','$scope',function($state,cache,$http,$scope){
+    if(cache.get('name')){
+      $scope.name=cache.get('name');
+      $scope.image=cache.get('image');
+    }
+    $scope.logout=function(){
+      cache.remove('id');
+      cache.remove('name');
+      cache.remove('image');
+      $state.go('main');
+    }
+}])
+angular.module('app').controller('positionCtrl',['$q','$scope','$state','$http','cache',function($q,$scope,$state,$http,cache){
+  $scope.isLogin=!!cache.get('name');
+  function getPosition(){
+    var def=$q.defer();
+  $http.get('data/position.json?id='+$state.params.id).then(function(resp){
+    $scope.position=resp.data;
+    def.resolve(resp);
+    console.log($state.params.id);
+});
+  return def.promise;
+}
+function getCompany(id){
+  $http.get('data/company.json?id='+id).then(function(resp){
+    $scope.company=resp.data;
+  })
+}
+getPosition().then(function(obj){
+  getCompany(obj.companyId);
+})
+}])
+angular.module('app').controller('postCtrl',['$http','$scope',function($http,$scope){
+  $scope.tabList=[{
+    id:'all',
+    name:'全部'
+  },{
+    id:'pass',
+    name:'面试邀请'
+  },{
+    id:'fail',
+    name:'不合格'
+  }];
+  $http.get('data/myPost.json').then(function(resp){
+    var res=resp.data;
+    $scope.positionList=res;
+  });
+  $scope.filterObj={};
+  $scope.tClick=function(id,name){
+    switch(id){
+      case 'all':
+        delete $scope.filterObj.state;
+        break;
+      case 'pass':
+        $scope.filterObj.state='1';
+        break;   
+      case 'fail':
+        $scope.filterObj.state='-1';
+        break;
+      default: 
+    }
+  }
+}])
+angular.module('app').controller('rejistorCtrl',['$interval','$http','$scope','$state',function($interval,$http,$scope,$state){
+  $scope.submit=function(){
+    $http.post('data/regist.json',$scope.user).then(function(resp){
+      console.log(resp);
+      $state.go('login');
+    })
+  }
+  var count=60;
+  $scope.fn1=function(){
+    $http.get('data/code.json').then(function(resp){
+      console.log(resp);
+      var res=resp.data;
+      if(1===res.state){
+        count=60;
+       var interval= $interval(function(){
+          $scope.time='60s';
+          if(count<=0){
+            $interval.cancel(interval);
+            $scope.time='';
+            return;
+          }else{
+          count--;
+          $scope.time=count+'s';
+          }
+        },1000);
+      }
+    })
+  }
+}])
+angular.module('app')
+.controller('searchCtrl',['$scope','$http','dict',function($scope,$http,dict){
+  $scope.name='';
+  $scope.search=function(){
+  $http.get('data/positionList.json?name='+$scope.name).then(function(resp){
+    $scope.positionList=resp.data;
+  });
+}
+$scope.search();
+$scope.sheet={};
+$scope.tabList=[{
+  id:'city',
+  name:'城市'
+},{
+  id:'salary',
+  name:'薪水'
+},{
+  id:'scale',
+  name:'公司规模'
+}];
+$scope.filterObj={};
+var tabId='';
+$scope.tClick=function(id,name){
+  tabId=id;
+  $scope.sheet.list=dict[id];
+  $scope.sheet.visible=true;
+};
+$scope.sClick=function(id,name){
+  if(id){
+    angular.forEach($scope.tabList,function(item){
+      if(item.id===tabId){
+        item.name=name;
+      }
+    });
+    $scope.filterObj[tabId+'Id']=id;
+  }else{
+    delete $scope.filterObj[tabId+'Id'];
+    angular.forEach($scope.tabList,function(item){
+      if(item.id===tabId){
+        switch(item.id){
+          case 'city':
+           item.name="城市";
+           break;
+          case 'salary':
+           item.name="薪水";
+           break;
+          case 'scale':
+           item.name="公司规模";
+           break;
+           default:
+        }
+      }
+  });
+}
+}
 }])
 angular.module('app').directive('appCompany',[function(){
   return {
@@ -224,201 +419,6 @@ angular.module('app').directive('appTab',[function(){
       }
     }
   }
-}])
-angular.module('app')
-.controller('companyCtrl',['$http','$state','$scope',function($http,$state,$scope){
-  $http.get('/data/company.json?id='+$state.params.id).then(function(resp){
-    $scope.company=resp.data;
-  })
-}])
-angular.module('app').controller('favoriteCtrl',['$http','$scope',function($http,$scope){
-  $http.get('data/myFavorite.json').then(function(resp){
-    var res=resp.data;
-    $scope.list=res;
-  })
-}])
-angular.module('app').controller('loginCtrl',['$http','$scope','$state','cache',function($http,$scope,$state,cache){
-  $scope.submit=function(){
-    $http.post('data/login.json',$scope.user).then(function(resp){
-      var res=resp.data;
-      cache.put('id',res.id);
-      cache.put('name',res.name);
-      cache.put('image',res.image);
-      $state.go('main');
-    })
-  }
-}])
-angular.module('app')
-.controller('mainCtrl',['$scope','$http',function($scope,$http){
-  $http.get('/data/positionList.json').then(function(resp){
-    console.log(resp);
-    $scope.list=resp.data;
-  })
-  // $scope.list=[{
-  //   name:'销售',
-  //   companyName:'千度',
-  //   imgSrc:'image/company-3.png',
-  //   city:'上海',
-  //   industry:'互联网',
-  //   time:'2017-4-10 11:05',
-  //   id:'1'
-  // },{
-  //   id:'2',
-  //  name:'WEB前端',
-  //   companyName:'慕课网',
-  //   imgSrc:'image/company-1.png',
-  //   industry:'互联网',
-  //   city:'北京',
-  //   time:'2017-4-5 01:10'
-  // }];
-}])
-angular.module('app').controller('meCtrl',['$state','cache','$http','$scope',function($state,cache,$http,$scope){
-    if(cache.get('name')){
-      $scope.name=cache.get('name');
-      $scope.image=cache.get('image');
-    }
-    $scope.logout=function(){
-      cache.remove('id');
-      cache.remove('name');
-      cache.remove('image');
-      $state.go('main');
-    }
-}])
-angular.module('app').controller('positionCtrl',['$q','$scope','$state','$http','cache',function($q,$scope,$state,$http,cache){
-  $scope.isLogin=!!cache.get('name');
-  function getPosition(){
-    var def=$q.defer();
-  $http.get('/data/position.json?id='+$state.params.id).then(function(resp){
-    $scope.position=resp.data;
-    def.resolve(resp);
-    console.log($state.params.id);
-});
-  return def.promise;
-}
-function getCompany(id){
-  $http.get('data/company.json?id='+id).then(function(resp){
-    $scope.company=resp.data;
-  })
-}
-getPosition().then(function(obj){
-  getCompany(obj.companyId);
-})
-}])
-angular.module('app').controller('postCtrl',['$http','$scope',function($http,$scope){
-  $scope.tabList=[{
-    id:'all',
-    name:'全部'
-  },{
-    id:'pass',
-    name:'面试邀请'
-  },{
-    id:'fail',
-    name:'不合格'
-  }];
-  $http.get('data/myPost.json').then(function(resp){
-    var res=resp.data;
-    $scope.positionList=res;
-  });
-  $scope.filterObj={};
-  $scope.tClick=function(id,name){
-    switch(id){
-      case 'all':
-        delete $scope.filterObj.state;
-        break;
-      case 'pass':
-        $scope.filterObj.state='1';
-        break;   
-      case 'fail':
-        $scope.filterObj.state='-1';
-        break;
-      default: 
-    }
-  }
-}])
-angular.module('app').controller('rejistorCtrl',['$interval','$http','$scope','$state',function($interval,$http,$scope,$state){
-  $scope.submit=function(){
-    $http.post('/data/regist.json',$scope.user).then(function(resp){
-      console.log(resp);
-      $state.go('login');
-    })
-  }
-  var count=60;
-  $scope.fn1=function(){
-    $http.get('/data/code.json').then(function(resp){
-      console.log(resp);
-      var res=resp.data;
-      if(1===res.state){
-        count=60;
-       var interval= $interval(function(){
-          $scope.time='60s';
-          if(count<=0){
-            $interval.cancel(interval);
-            $scope.time='';
-            return;
-          }else{
-          count--;
-          $scope.time=count+'s';
-          }
-        },1000);
-      }
-    })
-  }
-}])
-angular.module('app')
-.controller('searchCtrl',['$scope','$http','dict',function($scope,$http,dict){
-  $scope.name='';
-  $scope.search=function(){
-  $http.get('/data/positionList.json?name='+$scope.name).then(function(resp){
-    $scope.positionList=resp.data;
-  });
-}
-$scope.search();
-$scope.sheet={};
-$scope.tabList=[{
-  id:'city',
-  name:'城市'
-},{
-  id:'salary',
-  name:'薪水'
-},{
-  id:'scale',
-  name:'公司规模'
-}];
-$scope.filterObj={};
-var tabId='';
-$scope.tClick=function(id,name){
-  tabId=id;
-  $scope.sheet.list=dict[id];
-  $scope.sheet.visible=true;
-};
-$scope.sClick=function(id,name){
-  if(id){
-    angular.forEach($scope.tabList,function(item){
-      if(item.id===tabId){
-        item.name=name;
-      }
-    });
-    $scope.filterObj[tabId+'Id']=id;
-  }else{
-    delete $scope.filterObj[tabId+'Id'];
-    angular.forEach($scope.tabList,function(item){
-      if(item.id===tabId){
-        switch(item.id){
-          case 'city':
-           item.name="城市";
-           break;
-          case 'salary':
-           item.name="薪水";
-           break;
-          case 'scale':
-           item.name="公司规模";
-           break;
-           default:
-        }
-      }
-  });
-}
-}
 }])
 angular.module('app').filter('filterByObj',[function(){
   return function(list,obj){
